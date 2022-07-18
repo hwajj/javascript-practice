aa9da28f847c4884bc33cd031a7a8247cd0815f7
Jeong hwajeong <azamwara@gmail.com>
2022-07-18 18:20:54 +0900

    Make read-more button fetch data

--- coding_apple/script.js
+++ coding_apple/script.js
@@ -46,3 +46,39 @@ function addOptionList(dom, arr) {
     dom.append(option);
   });
 }
+let cardList = [];
+let $cardBox = document.querySelector('.card-box');
+fetchAndShow('https://codingapple1.github.io/js/more1.json', $cardBox);
+
+let $more = document.querySelector('.show-more');
+
+$more.addEventListener('click', () =>
+  fetchAndShow('https://codingapple1.github.io/js/more2.json', $cardBox)
+);
+
+function fetchAndShow(url, element) {
+  fetch(url)
+    .then((res) => res.json())
+    .then(function (data) {
+      let cardHtml;
+      console.log(data);
+      data.map((newData, i) => {
+        console.log(newData);
+        if (!cardList.find((item) => item.id === newData.id)) {
+          cardList.push(newData);
+          cardHtml = `<div class="col-sm-4">
+                        <img src="https://via.placeholder.com/600" class="w-100">
+                        <h5>${newData.title}</h5>
+                        <p>가격 : ${newData.price}</p>
+                        </div>
+                        `;
+          element.insertAdjacentHTML('beforeend', cardHtml);
+        } else {
+          return;
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log('실패함');
+    });
+}
